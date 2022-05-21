0d70918fd757d46c1b817e89660d17b0f067c0ec
AI_04_김아영 <83590357+kimmaa@users.noreply.github.com>
2022-05-22 00:32:29 +0900

    2022-05-22 TIL
    MySQL 에서 EXCEPT (차집합)대신 사용하는 방법 숙지/ INTERSECT(교집합) 대신 사용하는 방법 숙지

--- SQL_TIL_WEEK2_prac3.sql
+++ SQL_TIL_WEEK2_prac3.sql
@@ -1,3 +1,4 @@
+USE sakila;
 -- 문제1번) 영화 배우가, 영화 180분 이상의 길이의 영화에 출연하거나, 영화의 rating 이 R인 등급에 해당하는 영화에 출연한 영화 배우에 대해서, 
 -- 영화 배우 ID 와 (180분이상 / R등급영화)에 대한 Flag 컬럼을 알려주세요.
 -- - film_actor 테이블와 film 테이블을 이용하세요. union, unionall, intersect, except 중 상황에 맞게 사용해주세요.
@@ -45,13 +46,24 @@ WHERE  film_id IN (SELECT film_id
 
 -- 문제3번) G 등급에 해당하는 필름을 찍었으나, 영화를 20편이상 찍지 않은 영화배우의 ID 를 확인해주세요.
 -- - film_actor 테이블와 film 테이블을 이용하세요. union, unionall, intersect, except 중 상황에 맞게 사용해주세요.
-/*https://yahwang.github.io/posts/52*/
+SELECT a.actor_id,
+       f.rating,
+       Count(fa.film_id)
+FROM   film f
+       JOIN film_actor fa
+         ON f.film_id = fa.film_id
+       LEFT JOIN actor a
+              ON fa.actor_id = a.actor_id
+WHERE  f.film_id IN (SELECT f.film_id
+                     FROM   film f
+                     WHERE  f.rating = 'G')
+GROUP  BY a.actor_id
+HAVING NOT Count(DISTINCT fa.film_id) >= 20;
+
+-- 문제4번) 필름 중에서,  필름 카테고리가 Action, Animation, Horror 에 해당하지 않는 필름 아이디를 알려주세요.
+-- - category 테이블을 이용해서 알려주세요.
 
 /*
-문제4번) 필름 중에서,  필름 카테고리가 Action, Animation, Horror 에 해당하지 않는 필름 아이디를 알려주세요.
-
-- category 테이블을 이용해서 알려주세요.
-
 문제5번) Staff  의  id , 이름, 성 에 대한 데이터와 , Customer 의 id, 이름 , 성에 대한 데이터를  하나의  데이터셋의 형태로 보여주세요.
 
 - 컬럼 구성 : id, 이름 , 성, flag (직원/고객여부) 로 구성해주세요.
